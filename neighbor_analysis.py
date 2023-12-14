11485d300ec8457ff479d25408bcf7bf9a397265
weihuaguo <gwh120104@gmail.com>
2023-12-14 14:35:52 -0800

    Finished the fixed radius counting

---
+++ neighbor_analysis.py
@@ -0,0 +1,60 @@
+## Weihua Guo, Ph.D.
+## 12/14/2023, Sat., 09:25 PM
+
+import os
+os.system('clear')
+
+import glob
+import numpy as np
+import pandas as pd
+import datetime as dt
+import itertools as its
+import scipy.spatial.distance as ssd
+
+download_dir = "..."# NOTE: Please change the ... into the directory you were saving the data
+download_dir = "/home/weihua/mnts/smb_plee/Group/weihua/tdln_cold_hot_data_hub/ihc"# NOTE: Please change the ... into the directory you were saving the data
+
+panel_dir = download_dir+"/mDC_Th_Panel"
+in_dir = panel_dir+"/annotated_detections"
+fr_dir = panel_dir+"/fixed_radius"
+
+fid='*_extracted_detection.csv'
+all_detect_files=glob.glob(in_dir+'/'+fid)
+frs=[10,20,30,40,50]
+
+print("Start to extract the spatial information...\n")
+for idf in all_detect_files:
+    ipid=idf.split('/')[-1].split('_ex')[0]
+    print("Processing "+ipid)
+    print("\tReading the QuPath output...")
+    tst=dt.datetime.now()
+    tmp_df=pd.read_csv(idf, index_col=0)
+    print(tmp_df)
+    print(tmp_df.columns.tolist())
+    '''    nn_df = tmp_df[['Image', 'Name', 'Class', 'Parent', 'ROI', 'Centroid X µm', 'Centroid Y µm', 'phenotype']]
+    for iph in nn_df['phenotype'].unique():
+        nn_df[iph]=0
+    for irnn in range(tmp_df.shape[0]):
+        tmp_x=tmp_df['Centroid X µm'].iloc[ir]
+        tmp_y=tmp_df['Centroid Y µm'].iloc[ir]
+        for iph in nn_df['phenotype'].unique():
+'''
+    for ifr in frs:
+        fr_df = tmp_df[['Image', 'Name', 'Class', 'Parent', 'ROI', 'Centroid X µm', 'Centroid Y µm', 'phenotype']]
+        fr_df['fixed_r'] = ifr
+        for iph in fr_df['phenotype'].unique():
+            fr_df[iph]=0
+        print(fr_df)
+        for ir in range(tmp_df.shape[0]):
+            print(ir)
+            tmp_x=tmp_df['Centroid X µm'].iloc[ir]
+            tmp_y=tmp_df['Centroid Y µm'].iloc[ir]
+
+            x_mask=(tmp_df['Centroid X µm']>=tmp_x-ifr) & (tmp_df['Centroid X µm']<=tmp_x+ifr) & (tmp_df['Centroid X µm']!=tmp_x)
+            y_mask=(tmp_df['Centroid Y µm']>=tmp_y-ifr) & (tmp_df['Centroid Y µm']<=tmp_y+ifr) & (tmp_df['Centroid Y µm']!=tmp_y)
+            
+            tmp_nghb_df = tmp_df.loc[x_mask&y_mask]
+            if tmp_nghb_df.shape[0]>0:
+                for inghb in range(tmp_nghb_df.shape[0]):
+                    fr_df[tmp_nghb_df['phenotype'].iloc[inghb]].iloc[ir] += 1
+        fr_df.to_csv(fr_dir+"/"+ipid+"_fr"+str(ifr)+"_cell_count.csv")
