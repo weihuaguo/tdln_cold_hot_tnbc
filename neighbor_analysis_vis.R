316343a84afd86bd5ab52d81a6d63a723d141484
weihuaguo <gwh120104@gmail.com>
2024-01-13 13:35:39 -0800

    Finished the nearest neighborhood analysis

--- neighbor_analysis_vis.R
+++ neighbor_analysis_vis.R
@@ -16,10 +16,203 @@ nn_dir <- paste(panel_dir, "nearest_neighbors", sep = "/")
 frs <- c(10,20,30,40,50)
 smp_df <- as.data.frame(read_excel(paste(panel_dir, "sample_annotation_ihc_discovery.xlsx", sep = "/")))
 
-fr_flag <- T
+fr_flag <- F
 merge_fr_flag <- F
-vis_fr_flag <- T
-nn_flag <- F
+vis_fr_flag <- F
+nn_flag <- T
+merge_nn_flag <- F
+vis_nn_flag <- T
+
+if (nn_flag) {
+	if (merge_nn_flag) {
+		c <- 0
+		all_res <- list.files(nn_dir, pattern = "nearest_neighbor_distances")
+		for (ifile in all_res) {
+			cat(ifile, "\n")
+			df <- read.csv(paste(nn_dir, ifile, sep = "/"), row.names = 1, check.names = F)
+			mdc_df <- df[df$phenotype == "mDC",]
+			mdc_df$th21_flag <- ifelse(mdc_df$Th2 > mdc_df$Th1, "Th1", "Th2")
+			tdist_df <- mdc_df[,c("CD8+", "Th2", "Th1")]
+			adist_df <- mdc_df[,c("CD8+", "Th2", "Th1", "mDC")]
+
+			mdc_df$t_flag <- colnames(tdist_df)[apply(tdist_df, 1, which.min)]
+			mdc_df$a_flag <- colnames(adist_df)[apply(adist_df, 1, which.min)]
+			if (c == 0) {
+				merge_mdc_df <- mdc_df
+			} else {
+				merge_mdc_df <- rbind(merge_mdc_df, mdc_df)
+			}
+			c <- c+1
+		}
+		write.csv(merge_mdc_df, paste(fr_dir, "/merge_nn_mdc_df_cellwise.csv", sep = ""))
+	}
+	if (vis_nn_flag) {
+		mdc_df <- read.csv(paste(fr_dir, "/merge_nn_mdc_df_cellwise.csv", sep = ""), row.names = 1, check.names = F)
+		print(head(mdc_df))
+
+		#TODO: distance ratio calculation
+		dist_ratio_df <- mdc_df
+		dist_ratio_df$th21_dr <- dist_ratio_df$Th2/dist_ratio_df$Th1
+
+		dist_ratio_df$sld_id <- str_replace_all(str_split_fixed(dist_ratio_df$Image, "x", n = 2)[,1], "-", "_")
+		gath_df <- merge(dist_ratio_df, smp_df, by = "sld_id", all.x = T)
+
+		smpl_avg_df <- gath_df %>%
+			group_by(cohort, sld_id) %>%
+			summarise(n = n(),
+				  mean = mean(th21_dr, na.rm = T),
+				  median = median(th21_dr, na.rm = T),
+				  sd = sd(th21_dr, na.rm = T),
+				  min = min(th21_dr, na.rm = T),
+				  max = max(th21_dr, na.rm = T),
+				  se = sd/sqrt(n),
+				  mean_se_upper = mean+se,
+				  mean_se_lower = mean-se)
+		write.csv(smpl_avg_df, paste(nn_dir, "/merge_nn_descriptive_stat_samplewise_th21_dist_ratio.csv", sep = ""))
+
+		box_gg <- ggplot(smpl_avg_df, aes(x = cohort, y = mean, color = cohort)) +
+			geom_boxplot(outlier.shape = NA) +
+			geom_point(position = position_jitterdodge(dodge.width = 0.7), alpha = 0.666666) +
+			stat_compare_means(aes(group = cohort), label = "p.format") +
+			labs(y = "Distance ratio between nearest Th2 and Th1, sample wise") +
+			theme_bw()
+		ggsave(paste(nn_dir, "/merge_nn_boxplot_th21_dist_ratio_samplewise.png", sep = ""), dpi = 300, width = 3, height = 6)
+
+		cell_avg_df <- gath_df %>%
+			group_by(cohort) %>%
+			summarise(n = n(),
+				  mean = mean(th21_dr, na.rm = T),
+				  median = median(th21_dr, na.rm = T),
+				  sd = sd(th21_dr, na.rm = T),
+				  min = min(th21_dr, na.rm = T),
+				  max = max(th21_dr, na.rm = T),
+				  se = sd/sqrt(n),
+				  mean_se_upper = mean+se,
+				  mean_se_lower = mean-se)
+		write.csv(cell_avg_df, paste(nn_dir, "/merge_nn_descriptive_stat_cellwise_th21_dist_ratio.csv", sep = ""))
+
+
+		box_gg <- ggplot(gath_df, aes(x = cohort, y = th21_dr, color = cohort)) +
+			geom_boxplot(outlier.shape = NA) +
+			stat_compare_means(aes(group = cohort), label = "p.format") +
+			labs(y = "Distance ratio between nearest Th2 and Th1, cellwise") +
+			theme_bw()
+		ggsave(paste(nn_dir, "/merge_nn_boxplot_th21_dist_ratio_cellwise.png", sep = ""), dpi = 300, width = 3, height = 6)
+
+		gath_df <- gather(mdc_df, "nearest_cell", "dist", c("mDC", "CD8+", "Th2", "Th1"))
+		print(head(gath_df))
+		gath_df$sld_id <- str_replace_all(str_split_fixed(gath_df$Image, "x", n = 2)[,1], "-", "_")
+		gath_df <- merge(gath_df, smp_df, by = "sld_id", all.x = T)
+
+		smpl_avg_df <- gath_df %>%
+			group_by(cohort, nearest_cell, sld_id) %>%
+			summarise(n = n(),
+				  mean = mean(dist, na.rm = T),
+				  median = median(dist, na.rm = T),
+				  sd = sd(dist, na.rm = T),
+				  min = min(dist, na.rm = T),
+				  max = max(dist, na.rm = T),
+				  se = sd/sqrt(n),
+				  mean_se_upper = mean+se,
+				  mean_se_lower = mean-se)
+		write.csv(smpl_avg_df, paste(nn_dir, "/merge_nn_descriptive_stat_samplewise.csv", sep = ""))
+
+		box_gg <- ggplot(smpl_avg_df, aes(x = cohort, y = mean, color = cohort)) +
+			geom_boxplot(outlier.shape = NA) +
+			geom_point(position = position_jitterdodge(dodge.width = 0.7), alpha = 0.666666) +
+			stat_compare_means(aes(group = cohort), label = "p.format") +
+			facet_wrap(~nearest_cell, nrow = 1, scales = "free") +
+			labs(y = "Distance of nearest cells, sample_wise") +
+			theme_bw()
+		ggsave(paste(nn_dir, "/merge_nn_boxplot_dist_samplewise.png", sep = ""), dpi = 300, width = 6, height = 6)
+
+
+		cell_avg_df <- gath_df %>%
+			group_by(cohort, nearest_cell) %>%
+			summarise(n = n(),
+				  mean = mean(dist, na.rm = T),
+				  median = median(dist, na.rm = T),
+				  sd = sd(dist, na.rm = T),
+				  min = min(dist, na.rm = T),
+				  max = max(dist, na.rm = T),
+				  se = sd/sqrt(n),
+				  mean_se_upper = mean+se,
+				  mean_se_lower = mean-se)
+		write.csv(cell_avg_df, paste(nn_dir, "/merge_nn_descriptive_stat_cellwise.csv", sep = ""))
+
+		box_gg <- ggplot(gath_df, aes(x = cohort, y = dist, color = cohort)) +
+			geom_boxplot(outlier.shape = NA) +
+#			geom_point(position = position_jitterdodge(dodge.width = 0.7), alpha = 0.001) +
+			stat_compare_means(aes(group = cohort), label = "p.format") +
+			facet_wrap(~nearest_cell, nrow = 1, scales = "free") +
+			labs(y = "Distance of nearest cells, cell-wise") +
+			theme_bw()
+		ggsave(paste(nn_dir, "/merge_nn_boxplot_dist_cellwise.png", sep = ""), dpi = 300, width = 6, height = 6)
+
+		a_sum_df <- mdc_df %>%
+			group_by(Image) %>%
+			mutate(n_mdc = n())
+		a_sum_df <- a_sum_df %>%
+			group_by(Image, a_flag) %>%
+			summarize(n_a = n(), n_mdc = mean(n_mdc)) %>%
+			mutate(rel_a = n_a/n_mdc*100)
+		print(a_sum_df)
+		a_sum_df$sld_id <- str_replace_all(str_split_fixed(a_sum_df$Image, "x", n = 2)[,1], "-", "_")
+		sum_df <- merge(a_sum_df, smp_df, by = "sld_id", all.x = T)
+		sum_df[is.na(sum_df)] <- 0.0
+		box_gg <- ggplot(sum_df, aes(x = cohort, y = rel_a, color = cohort)) +
+			geom_boxplot() +
+			geom_point(position = position_jitterdodge(dodge.width = 0.7)) +
+			stat_compare_means(aes(group = cohort), label = "p.format") +
+			facet_wrap(~a_flag, nrow = 1, scales = "free") +
+			labs(y = "mDC percentage (%) (Th2, Th1, CD8+ and mDC)") +
+			theme_bw()
+		ggsave(paste(nn_dir, "/merge_nn_boxplot_sum_rel_mdc_all.png", sep = ""), dpi = 300, width = 6, height = 6)
+
+
+		t_sum_df <- mdc_df %>%
+			group_by(Image) %>%
+			mutate(n_mdc = n())
+		t_sum_df <- t_sum_df %>%
+			group_by(Image, t_flag) %>%
+			summarize(n_t = n(), n_mdc = mean(n_mdc)) %>%
+			mutate(rel_t = n_t/n_mdc*100)
+		print(t_sum_df)
+		t_sum_df$sld_id <- str_replace_all(str_split_fixed(t_sum_df$Image, "x", n = 2)[,1], "-", "_")
+		sum_df <- merge(t_sum_df, smp_df, by = "sld_id", all.x = T)
+		sum_df[is.na(sum_df)] <- 0.0
+		box_gg <- ggplot(sum_df, aes(x = cohort, y = rel_t, color = cohort)) +
+			geom_boxplot() +
+			geom_point(position = position_jitterdodge(dodge.width = 0.7)) +
+			stat_compare_means(aes(group = cohort), label = "p.format") +
+			facet_wrap(~t_flag, nrow = 1, scales = "free") +
+			labs(y = "mDC percentage (%) (Th2, Th1, CD8+ only)") +
+			theme_bw()
+		ggsave(paste(nn_dir, "/merge_nn_boxplot_sum_rel_mdc_t_only.png", sep = ""), dpi = 300, width = 4, height = 6)
+
+		th21_sum_df <- mdc_df %>%
+			group_by(Image) %>%
+			mutate(n_mdc = n())
+		th21_sum_df <- th21_sum_df %>%
+			group_by(Image, th21_flag) %>%
+			summarize(n_th21 = n(), n_mdc = mean(n_mdc)) %>%
+			mutate(rel_th21 = n_th21/n_mdc*100)
+		print(th21_sum_df)
+		th21_sum_df$sld_id <- str_replace_all(str_split_fixed(th21_sum_df$Image, "x", n = 2)[,1], "-", "_")
+		sum_df <- merge(th21_sum_df, smp_df, by = "sld_id", all.x = T)
+		sum_df[is.na(sum_df)] <- 0.0
+		box_gg <- ggplot(sum_df, aes(x = cohort, y = rel_th21, color = cohort)) +
+			geom_boxplot() +
+			geom_point(position = position_jitterdodge(dodge.width = 0.7)) +
+			stat_compare_means(aes(group = cohort), label = "p.format") +
+			facet_wrap(~th21_flag, nrow = 1, scales = "free") +
+			labs(y = "mDC percentage (%) (Th2, Th1 only)") +
+			theme_bw()
+		ggsave(paste(nn_dir, "/merge_nn_boxplot_sum_rel_mdc_th21_only.png", sep = ""), dpi = 300, width = 4, height = 6)
+
+
+	}
+}
 
 if (fr_flag) {
 	if (merge_fr_flag) {
@@ -93,6 +286,23 @@ if (fr_flag) {
 			sum_df$r_th21_ratio <- sum_df$r_th2/sum_df$r_th1
 #			print(head(sum_df))
 #			q(save = "no")
+
+			box_gg <- ggplot(sum_df, aes(x = cohort, y = r_th1, color = cohort)) +
+				geom_boxplot() +
+				geom_point(position = position_jitterdodge(dodge.width = 0.7)) +
+				stat_compare_means(aes(group = cohort), label = "p.format") +
+				labs(title = paste("Fixed radius:", ifr), y = "Th1 around mDC%") +
+				theme_bw()
+			ggsave(paste(fr_dir, "/merge_fr_", ifr, "_boxplot_sum_rel_th1.png", sep = ""), dpi = 300, width = 3, height = 6)
+
+			box_gg <- ggplot(sum_df, aes(x = cohort, y = r_th2, color = cohort)) +
+				geom_boxplot() +
+				geom_point(position = position_jitterdodge(dodge.width = 0.7)) +
+				stat_compare_means(aes(group = cohort), label = "p.format") +
+				labs(title = paste("Fixed radius:", ifr), y = "Th2 around mDC%") +
+				theme_bw()
+			ggsave(paste(fr_dir, "/merge_fr_", ifr, "_boxplot_sum_rel_th2.png", sep = ""), dpi = 300, width = 3, height = 6)
+
 
 			box_gg <- ggplot(sum_df, aes(x = cohort, y = th21_ratio, color = cohort)) +
 				geom_boxplot() +
